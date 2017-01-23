d826896520bc8b00b9a483fc7b77f612670960db
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-23 13:42:22 +0500

    added request method to services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
-
+import {HackernewsApiService} from './hackernews-api.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +22,7 @@ import { ItemComponent } from './item/item.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HackernewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
+import { HackernewsApiService } from '../hackernews-api.service';
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.scss']
 })
 export class StoriesComponent implements OnInit {
-items:number[];
+items;
   constructor() {
-    this.items=Array(30);
      }
 
   ngOnInit() {
