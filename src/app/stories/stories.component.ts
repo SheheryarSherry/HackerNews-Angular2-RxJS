6f2acf4f56159e1532b26d10530484e21e577d2d
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-28 11:07:56 +0500

    routes updates

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {ActivatedRoute} from '@angular/router';
 import { HackernewsApiService } from '../hackernews-api.service';
+
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
@@ -24,13 +25,15 @@ listStart:number;
       .data
       .subscribe(data => this.storiesType = (data as any).storiesType);
 
-    this.pageSub = this.route.params.subscribe(params => {
-      this.pageNum = +params['page'] ? +params['page'] : 1;
-      this._hackerNewsApiService.fetchStories(this.storiesType, this.pageNum)
-                              .subscribe(
-                                items => this.items = items,
-                                error => console.log('Error fetching' + this.storiesType + 'stories'),
-                                () => this.listStart = ((this.pageNum - 1) * 30) + 1);
+    this.pageSub = this.route.params.subscribe(params => { this.pageNum = +params['page'] ? +params['page'] : 1;
+    this._hackerNewsApiService.fetchStories(this.storiesType, this.pageNum)
+                            .subscribe(
+                              items => this.items = items,
+                              error => console.log('Error fetching' + this.storiesType + 'stories'),
+                              () => {
+                                this.listStart = ((this.pageNum - 1) * 30) + 1;
+                                window.scrollTo(0, 0);
+                              });
     });
   }
 }
